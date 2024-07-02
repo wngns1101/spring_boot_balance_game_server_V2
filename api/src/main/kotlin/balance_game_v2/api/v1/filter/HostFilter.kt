73c1e93ea3c4a0c47f9ebca3e1ce8813fd8a6083
wngns1101 <wngns0812@gmail.com>
2024-07-02 22:34:20 +0900

    feat/hostfilter 추가

---
+++ api/src/main/kotlin/balance_game_v2/api/v1/filter/HostFilter.kt
@@ -0,0 +1,28 @@
+package balance_game_v2.api.v1.filter
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+
+class HostFilter : Filter {
+    @Value("\${dns}")
+    private val dns: String? = null
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain) {
+        val req: HttpServletRequest = request as HttpServletRequest
+        val res: HttpServletResponse = response as HttpServletResponse
+
+        val host = req.getHeader("Host")
+
+        if (!host.equals("localhost") && !host.equals(dns)) {
+            response.getWriter().write("Forbidden")
+            response.getWriter().flush()
+            return
+        }
+
+        chain.doFilter(request, response)
+    }
+}
--- api/src/main/kotlin/balance_game_v2/config/ApiFilterConfig.kt
+++ api/src/main/kotlin/balance_game_v2/config/ApiFilterConfig.kt
@@ -1,6 +1,7 @@
 package balance_game_v2.config
 
 import balance_game_v2.api.v1.filter.ApiFilter
+import balance_game_v2.api.v1.filter.HostFilter
 import balance_game_v2.api.v1.user.application.TokenManager
 import balance_game_v2.api.v1.user.http.common.CookieUtils
 import org.springframework.boot.web.servlet.FilterRegistrationBean
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 
 @Configuration
-class ApiFilterConfig() {
+class ApiFilterConfig {
     private val includeTokenFilterPaths = arrayOf(
         "$USER_V2_PREFIX/*",
         "$BOARD_V2_PREFIX/*",
@@ -18,9 +19,16 @@ class ApiFilterConfig() {
     @Bean
     fun tokenFilter(cookieUtils: CookieUtils, tokenManager: TokenManager): FilterRegistrationBean<ApiFilter> {
         val registrationBean = FilterRegistrationBean(ApiFilter(cookieUtils, tokenManager))
-        registrationBean.order = Integer.MIN_VALUE
         registrationBean.addUrlPatterns(*includeTokenFilterPaths)
-        registrationBean.order = Ordered.HIGHEST_PRECEDENCE
+        registrationBean.order = Ordered.LOWEST_PRECEDENCE + 10
+        return registrationBean
+    }
+
+    @Bean
+    fun hostFilter(): FilterRegistrationBean<HostFilter> {
+        val registrationBean = FilterRegistrationBean(HostFilter())
+        registrationBean.addUrlPatterns("/*")
+        registrationBean.order = Ordered.LOWEST_PRECEDENCE
         return registrationBean
     }
 }
