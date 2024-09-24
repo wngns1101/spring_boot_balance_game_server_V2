9e4a0cd37d17be511bc2c359afefb8a891b13341
wngns1101 <wngns0812@gmail.com>
2024-09-24 10:10:46 +0900

    feat: 참여한 게임 response 추가

--- domain/src/main/kotlin/balance_game_v2/domain/board/dto/ParticipatedBoardDTO.kt
+++ domain/src/main/kotlin/balance_game_v2/domain/board/dto/ParticipatedBoardDTO.kt
@@ -3,6 +3,7 @@ package balance_game_v2.domain.board.dto
 import balance_game_v2.domain.board.entity.Board
 
 data class ParticipatedBoardDTO(
+    val boardId: Long,
     val title: String,
     val keywords: List<String>,
     val introduce: String,
@@ -11,6 +12,7 @@ data class ParticipatedBoardDTO(
 
 fun Board.toParticipatedBoardDTO(isReviewExist: Boolean, keywords: List<String>): ParticipatedBoardDTO {
     return ParticipatedBoardDTO(
+        boardId = boardId!!,
         title = title,
         keywords = keywords,
         introduce = introduce,
